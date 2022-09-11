57cdc2bcd4cf2d1559b82f0c8e7bf51c8b8fc26e
grasbiel <bzsilva1998@gmail.com>
2022-09-11 20:26:30 -0300

    Component AddArea e ListItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import * as C from './App.styles';
+import AddArea from './components/AddArea';
 import { ListItem } from './components/ListItem';
 import { Item } from './types/Item';
 
@@ -13,16 +14,43 @@ const App = () => {
     },
 
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list]
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
   return (
 
     <C.Container>
       <C.Area>
         <C.Header> Lista de Tarefas</C.Header>
-        {list.map((item, index)=> (
-            
-            <ListItem key={index} item = {item}  />
-          ))}
-        
+
+        <AddArea onEnter={handleAddTask} />
+        {list.map((item, index) => (
+
+          <ListItem
+            key={index}
+            item={item}
+            onChange={handleTaskChange}
+          />
+
+        ))}
+
       </C.Area>
     </C.Container>
   );
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -3,19 +3,20 @@ import { Item } from '../../types/Item'
 import { useState } from 'react'
 
 type Props = {
-    item: Item
+    item: Item,
+    onChange: (id: number,  done: boolean) => void
 }
 
-export const ListItem = ({ item }: Props) => {
+export const ListItem = ({ item, onChange }: Props) => {
     const [isChecked, setIsChecked] = useState(item.done);
 
     return (
         <C.Container done = {isChecked}>
             <input
                 type="checkbox"
-                checked={isChecked}
-                onChange= {e => setIsChecked(e.target.checked)} />
-            <label>{item.name}</label>
+                checked={item.done}
+                onChange= {e => onChange(item.id, e.target.checked)} />
+            <label>{item.name} - {item.done.toString()} </label>
         </C.Container>
     )
 }
