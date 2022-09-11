48f6da6c94964af7bd69174a6b8b315f4c488c0b
grasbiel <bzsilva1998@gmail.com>
2022-09-11 20:28:37 -0300

    Correção exibição do status

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -8,15 +8,14 @@ type Props = {
 }
 
 export const ListItem = ({ item, onChange }: Props) => {
-    const [isChecked, setIsChecked] = useState(item.done);
-
+    
     return (
-        <C.Container done = {isChecked}>
+        <C.Container done = {item.done}>
             <input
                 type="checkbox"
                 checked={item.done}
                 onChange= {e => onChange(item.id, e.target.checked)} />
-            <label>{item.name} - {item.done.toString()} </label>
+            <label>{item.name} </label>
         </C.Container>
     )
 }
