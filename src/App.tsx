faaa2794dc213b695e91aa2dccbfe0253ec527c1
grasbiel <bzsilva1998@gmail.com>
2022-09-11 18:51:24 -0300

    Componente ListItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { ListItem } from './components/ListItem';
+import { Item } from './types/Item';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1, name: 'Comprar o pão na padaria', done: false
+    },
+    {
+      id: 2, name: 'Limpar o apartamento', done: true
+    },
+
+  ]);
   return (
+
     <C.Container>
       <C.Area>
-        <C.Header> 
-          Lista de Tarefas
-        </C.Header>
+        <C.Header> Lista de Tarefas</C.Header>
+        {list.map((item, index)=> (
+            
+            <ListItem key={index} item = {item}  />
+          ))}
+        
       </C.Area>
     </C.Container>
   );
